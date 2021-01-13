654c08f7c22d90dbcd7a8e110cd8bb0c9e8452e6
Chang <cnxiong03@gmail.com>
2021-01-12 23:00:32 -0500

    BarkingDog complete

---
+++ src/com/codewithchang/BarkingDog.java
@@ -0,0 +1,15 @@
+package com.codewithchang;
+
+public class BarkingDog {
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (!barking || hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        }
+        if (hourOfDay < 8 || hourOfDay > 22 && barking) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
